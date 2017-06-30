da13a4706d35575e17b4e4d7dc493099af589442
Kushal Hada <kushaldeveloper@gmail.com>
2017-06-30 10:04:08 -0400

    add twig
    WIP defiantly WIP

--- modules/custom/qbpl_volunteer/src/Controller/VolunteerController.php
+++ modules/custom/qbpl_volunteer/src/Controller/VolunteerController.php
@@ -6,8 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 class VolunteerController extends ControllerBase {
   public function content(){
     return array(
-      '#type' => 'markup',
-      '#markup' => $this->t('Volunteer with us today!'),
+      '#theme' => 'qbpl-volunteer',
     );
   }
 }
