1c36f0b0927c862a75037cdc26ace0a54caeee2c
Kushal Hada <kushaldeveloper@gmail.com>
2017-07-03 09:45:50 -0400

    add new routes inside volunteer
    volunteer/new and volunteer/old

--- modules/custom/qbpl_volunteer/src/Controller/VolunteerController.php
+++ modules/custom/qbpl_volunteer/src/Controller/VolunteerController.php
@@ -3,13 +3,34 @@ namespace Drupal\qbpl_volunteer\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-class VolunteerController extends ControllerBase {
-  public function content(){
-    $apple = 'steve jobs';
-    return array(
-      '#theme' => 'qbpl_volunteer', 
-      '#title' => 'this is the controller function content\'s title',
-      '#apple' => $apple,
-    );
-  }
+class VolunteerController extends ControllerBase
+{
+    public function content()
+    {
+        $apple = 'steve jobs';
+        return array(
+        '#theme' => 'qbpl_volunteer',
+        '#title' => 'this is the controller function content\'s title',
+        '#apple' => $apple,
+        );
+    }
+    public function welcome()
+    {
+        $apple = 'steve jobs';
+        return array(
+        '#theme' => 'qbpl_volunteer',
+        '#title' => 'this is the controller function welcome title',
+        '#apple' => $apple,
+        );
+    }
+
+    public function welcomeBack()
+    {
+        $apple = 'steve jobs';
+        return array(
+        '#theme' => 'qbpl_volunteer',
+        '#title' => 'this is the controller function welcome back title',
+        '#apple' => $apple,
+        );
+    }
 }
